6795b011da6c958b53a144fcc4c5a1d2e8c5ab2b
mf-ff99 <force.j.mark@gmail.com>
2023-09-07 19:37:04 -0400

    HomeView text edited for clarity

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -6,13 +6,16 @@
 <template>
     <div>
        <p>
-            welcome to airy
+        welcome to airy
        </p>
        <p>
         airy is a minimalist chat app that allows users to connect and share bits of information with each other
        </p>
        <p>
         you're also allowed to just air out your thoughts and feelings
+       </p>
+       <p>
+        airy isn't quite finished yet, but feel free to click around and see what you can do
        </p>
     </div>
 </template>
